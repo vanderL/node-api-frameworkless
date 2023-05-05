5419b6857555db18027e5e2ed84a414d364b6b4f
Vander Lima <vnd.vander@gmail.com>
2023-05-05 01:56:35 -0300

    working statefull

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url, } = request;
 
   if (method === "GET" && url === "/users") {
-    return response.end('GET de users');
+    return response.setHeader("Content-type", "aplication/json").end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return response.end('Create de users');
+    users.push({
+      id: "bd6b96b2-a75a-5400-afd3-de91df3d1285",
+      name: "Vander",
+      email: "test"
+    })
+
+    return response.end('Usuario estatico criado');
   }
 
   return response.end("404")
