f5a3a41d3261aa75e9d8bf733a82cc856449a90c
Vander Lima <vnd.vander@gmail.com>
2023-05-07 02:08:45 -0300

    Stream & Buffers

--- src/server.js
+++ src/server.js
@@ -2,18 +2,32 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url, } = request;
+
+  const buffers = []
+
+  for await (const chuck of request) {
+    buffers.push(chuck)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    request.body = null
+  }
 
   if (method === "GET" && url === "/users") {
     return response.setHeader("Content-type", "aplication/json").end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body
+
     users.push({
       id: "bd6b96b2-a75a-5400-afd3-de91df3d1285",
-      name: "Vander",
-      email: "test"
+      name,
+      email
     })
 
     return response.writeHead(201).end();
--- src/streams/stream-http-server.js
+++ src/streams/stream-http-server.js
@@ -14,8 +14,20 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverseNumberStream).pipe(res)
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chuck of req) {
+    buffers.push(chuck)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent)
+
+  // return req.pipe(new InverseNumberStream).pipe(res)
 })
 
 server.listen(3334, () => {
